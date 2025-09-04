7489bf19dbbec3d4545a777253df4237e0520499
Tushar <tuharbansal3366@gmail.com>
2025-09-05 01:01:40 +0530

    favicon genrated

--- src/app/layout.js
+++ src/app/layout.js
@@ -188,6 +188,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-DYD1ML9EZ0"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-DYD1ML9EZ0');
+            `,
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
