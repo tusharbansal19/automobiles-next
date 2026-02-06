64fc09ae0ff236c8fec5b59cf56c5b7795662d11
Tushar <tuharbansal3366@gmail.com>
2026-02-07 00:05:03 +0530

    fix: correct robots.js format for Next.js App Router

--- src/app/robots.js
+++ src/app/robots.js
@@ -3,7 +3,7 @@ export default function robots() {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/api/',
+      disallow: ['/api/', '/admin/', '/private/'],
     },
     sitemap: 'https://tusharautomobiles.me/sitemap.xml',
   }
