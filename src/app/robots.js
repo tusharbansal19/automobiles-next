136b201a8da62e615ebdfcda89b06c54e30a7c12
Tushar <tuharbansal3366@gmail.com>
2026-02-06 23:55:52 +0530

    simplify robots.js to minimal config

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,19 +1,10 @@
 export default function robots() {
   return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-        disallow: [
-          "/admin/",
-          "/profile/",
-          "/api/",
-          "/_next/",
-          "/_vercel/",
-        ],
-      },
-    ],
-    sitemap: "https://tusharautomobiles.me/sitemap.xml",
-    host: "tusharautomobiles.me",
-  };
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: '/api/',
+    },
+    sitemap: 'https://tusharautomobiles.me/sitemap.xml',
+  }
 }
