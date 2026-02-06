0b8b74ecf9039bf025fd451b441277a6e40b2536
Tushar <tuharbansal3366@gmail.com>
2026-02-06 23:30:37 +0530

    new sitemapo

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,89 +1,19 @@
 export default function robots() {
-  const baseUrl = "https://tusharautomobiles.me";
-
   return {
     rules: [
       {
         userAgent: "*",
-        allow: [
-          "/",
-          "/about",
-          "/cars", 
-          "/contact",
-          "/services",
-          "/performance",
-          "/Image/",
-          "/favicon.ico",
-          "/manifest.json",
-          "/sitemap.xml",
-          "/robots.txt"
-        ],
+        allow: "/",
         disallow: [
-          "/api/",
-          "/private/",
           "/admin/",
           "/profile/",
+          "/api/",
           "/_next/",
           "/_vercel/",
-          "/*.json$",
-          "/search?*",
-          "/cart?*",
-          "/checkout?*",
-          "/user/",
-          "/dashboard/",
-          "/temp/",
-          "/test/",
-          "/dev/",
-          "/staging/"
-        ],
-        crawlDelay: 1,
-      },
-      {
-        userAgent: "Googlebot",
-        allow: "/",
-        disallow: [
-          "/api/",
-          "/admin/",
-          "/profile/",
-          "/_next/",
-          "/_vercel/"
-        ],
-        crawlDelay: 0,
-      },
-      {
-        userAgent: "Bingbot",
-        allow: "/",
-        disallow: [
-          "/api/",
-          "/admin/",
-          "/profile/",
-          "/_next/",
-          "/_vercel/"
-        ],
-        crawlDelay: 1,
-      },
-      {
-        userAgent: "facebookexternalhit",
-        allow: "/",
-        disallow: [
-          "/api/",
-          "/admin/",
-          "/profile/"
         ],
       },
-      {
-        userAgent: "Twitterbot",
-        allow: "/",
-        disallow: [
-          "/api/",
-          "/admin/",
-          "/profile/"
-        ],
-      }
     ],
-    sitemap: `${baseUrl}/sitemap.xml`,
-    host: baseUrl.replace(/^https?:\/\//, ""),
+    sitemap: "https://tusharautomobiles.me/sitemap.xml",
+    host: "tusharautomobiles.me",
   };
 }
-
-
